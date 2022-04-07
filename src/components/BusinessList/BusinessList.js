fa77b1bab1935d6c7b041df5185879d105a044b9
Siskyy <marcbertelli7@gmail.com>
2022-04-07 18:50:42 +0100

    Business list init

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BusinessList.css';
+
+class BusinessList extends React.Component {
+    
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default BusinessList;
