962922164e4bebfbb0caf13bcb52d443e4d2b917
Siskyy <marcbertelli7@gmail.com>
2022-04-07 18:54:15 +0100

    BusinessList component w/ css

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './BusinessList.css';
 
+import Business from '../Business/Business';
+
 class BusinessList extends React.Component {
     
     render() {
         return (
-
+            <div className="businessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
         )
     }
 }
