b0f149da5fbd2c58d3e4bacc8e1d1a980c0542ff
Siskyy <marcbertelli7@gmail.com>
2022-04-07 20:17:02 +0100

    App jsx strcuture and SearchBar bug fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Business from './components/Business/Business';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-        <Business />
-    </div>
+    <>
+      <div className="App">
+        <SearchBar />
+        <BusinessList />
+      </div>
+    </>
   );
 }
 
