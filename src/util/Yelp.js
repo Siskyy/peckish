6c8a9c8b09cc2af7ddd980c229396cfb4a3d24d2
Siskyy <marcbertelli7@gmail.com>
2022-04-15 11:57:31 +0100

    yelp.js (unreachable) business object

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,9 @@
 const apiKey = "orJDnPMg5LVIwqKHsyr5-mj0BNyDInJX_HIC6unP9MdNODkJrrxeDl2b3wYtLNkkgyNZ599Vk1tcLNAEJs3hJaRcxExIMl7vdXNVzFD_8lmUEXCADKD_F33ekURZYnYx";
 
-const Yelp = {
+const yelp = {
     searchYelp(term, location, sortBy) {
         return {
-            fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?${term}=TERM&location=${location}&sort_by=${sortBy}`, {
+            fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
                 headers: {
                     Athorization: `Bearer ${apiKey}`
                 }
@@ -12,19 +12,23 @@ const Yelp = {
             }).then(jsonResponse => {
                 if (jsonResponse.businesses) {
                     return jsonResponse.businesses.map(business => {
-                        id: '',
-                        imageSrc: '',
-                        name: '',
-                        address: '',
-                        city: '',
-                        state: '',
-                        zipCode: '',
-                        category: '',
-                        rating: '',
-                        reviewCount: ''
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count
+                        }
                     });
                 }
             });
         };
     };
 };
+
+export default yelp;
